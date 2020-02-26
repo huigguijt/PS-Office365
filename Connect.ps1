bed477cca1658f1a44e44ff4ed71cafd03eaed52
huigguijt <44601326+huigguijt@users.noreply.github.com>
2020-02-26 16:45:15 +0100

    Delete Connect.ps1

--- Connect.ps1
+++
@@ -1,8 +0,0 @@
-$Credential = Get-Credential
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $Credential -Authentication Basic -AllowRedirection
-Import-PSSession $Session
-Import-Module MsOnline
-Connect-MSOLService -Credential $Credential
-Import-Module LyncOnlineConnector
-$session = New-CsOnlineSession -Credential $credential
-Import-PSSession $session
