97b86e18c71baa74af5518f7050661aef9ca0042
huigguijt <44601326+huigguijt@users.noreply.github.com>
2021-11-10 14:32:08 +0100

    Add files via upload

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,6 @@
+Set-Location "C:\Users\Guijt\OneDrive - Wortell O365 (1)\Scripts>"
+$host.ui.RawUI.WindowTitle = "Übershell"
+Clear-Host
+#Welcome message
+Write-Host "With great power comes great responsibility.
+" -foregroundcolor Yellow
